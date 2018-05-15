8d1d49bb66ec6a228c91c8b8fbdecf2c23bc5f37
slovenlyboy <slovenlyboy@gmail.com>
2018-05-15 10:22:51 +0900

    弾置き場プレハブ（空オブジェ）の追加

--- Assets/Scripts/Game/ElementObject/TestShot.cs
+++ Assets/Scripts/Game/ElementObject/TestShot.cs
@@ -28,41 +28,6 @@ namespace Play.Enemy
 		//発射カウント
 		[SerializeField]
 		float _shotCount;
-
-<<<<<<< HEAD
-		// Use this for initialization
-		void Start()
-		{
-			//発射カウントのリセット
-			_shotCount = _shotInterval;
-		}
-
-		// Update is called once per frame
-		void Update()
-		{
-			//発射カウントダウン
-			_shotCount -= Time.deltaTime;
-			//発射カウント0時
-			if (_shotCount <= 0)
-			{
-				//弾の方向設定（正面）
-				_bulletVel = transform.up;
-				//オフセットの設定(テストなので後日修正)
-				_shotOffset = transform.up * GetComponentInChildren<SpriteRenderer>().bounds.size.x / 1.2f;
-				//// 弾丸の複製
-				GameObject bullets = GameObject.Instantiate(_bullet) as GameObject;
-				// 弾速設定
-				bullets.GetComponent<Rigidbody2D>().velocity = _bulletVel * _bulletSpeed;
-				// 弾丸の位置を調整
-				bullets.transform.position = transform.position + _shotOffset;
-				//弾丸をゲームオブジェクトの子供に設定
-				bullets.transform.parent = transform;
-				//発車時間の再設定
-				_shotCount = _shotInterval;
-			}
-		}
-	}
-=======
         //弾置き場
         GameObject _bulletPlace;
 
@@ -109,5 +74,4 @@ namespace Play.Enemy
             }
         }
     }
->>>>>>> b79c3a8... テストショットに弾置き場参照追加、弾置き場があれば弾置き場の子供になるマス
 }
