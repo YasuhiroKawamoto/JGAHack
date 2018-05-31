194884c76ab05f9e55dae9aae72fe3d059055e07
やまげん <dqx_2527@yahoo.co.jp>
2018-05-31 14:19:01 +0900

    セレクト画面仮

---
+++ Assets/Scripts/Display/SelectDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util.Display
+{
+    public sealed class SelectDisplay : DisplayBase
+    {
+        // タイトルディスプレイ
+        [SerializeField]
+        private TitleDisplay _titleDisplay = null;
+
+        public override IEnumerator Enter()
+        {
+            yield return null;
+        }
+
+        public override void EnterComplete()
+        {
+            base.EnterComplete();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void KeyInput()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Display/TitleDisplay.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Util.Display
+{
+    public sealed class TitleDisplay : DisplayBase
+    {
+        // ステージセレクトディスプレイ
+        [SerializeField]
+        private SelectDisplay _selectDisplay = null;
+
+        public override IEnumerator Enter()
+        {
+            yield return null;
+        }
+
+        public override void EnterComplete()
+        {
+            base.EnterComplete();
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+        }
+
+        public override void KeyInput()
+        {
+
+        }
+    }
+}
