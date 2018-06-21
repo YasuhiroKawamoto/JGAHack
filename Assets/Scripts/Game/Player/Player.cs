c24e1fcd605f1127b179ae81b025a06a7f679cdd
やまげん <dqx_2527@yahoo.co.jp>
2018-06-21 14:00:09 +0900

    多重死亡しないように

--- Assets/Scripts/Game/Player/Player.cs
+++ Assets/Scripts/Game/Player/Player.cs
@@ -5,189 +5,189 @@ using Util.Sound;
 
 namespace Play
 {
-	public class Player : MonoBehaviour
-	{
-		[SerializeField]
-		private float _moveSpeed;
+    public class Player : MonoBehaviour
+    {
+        [SerializeField]
+        private float _moveSpeed;
 
-		private Rigidbody2D _rigidbody;
+        private Rigidbody2D _rigidbody;
 
-		private float _waitCount = 0;
+        private float _waitCount = 0;
 
 
-		public enum State
-		{
-			Alive,
-			Dead
-		}
+        public enum State
+        {
+            Alive,
+            Dead
+        }
 
-		[SerializeField]
-		private State _playerState = State.Alive;
+        [SerializeField]
+        private State _playerState = State.Alive;
 
-		public State PlayerState
-		{
-			get { return _playerState; }
-		}
+        public State PlayerState
+        {
+            get { return _playerState; }
+        }
 
-		// 向き
-		[SerializeField, Extensions.ReadOnly]
-		private Direction _direction = Direction.Front;
-		public Direction Dir { get { return _direction; } }
+        // 向き
+        [SerializeField, Extensions.ReadOnly]
+        private Direction _direction = Direction.Front;
+        public Direction Dir { get { return _direction; } }
 
-		void Start()
-		{
-			_rigidbody = GetComponent<Rigidbody2D>();
-		}
+        void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
 
-		void Update()
-		{
-			if (!InGameManager.IsInstance())
-			{
-				return;
-			}
+        void Update()
+        {
+            if (!InGameManager.IsInstance())
+            {
+                return;
+            }
 
-			if (InGameManager.Instance.GameState != InGameManager.State.Play)
-			{
-				// ゲームが開始していないときは移動しない
-				return;
-			}
+            if (InGameManager.Instance.GameState != InGameManager.State.Play)
+            {
+                // ゲームが開始していないときは移動しない
+                return;
+            }
 
-			// 死んでいるときは動かない
-			if (PlayerState == State.Dead)
-			{
-				_rigidbody.velocity = Vector2.zero;
-				return;
-			}
+            // 死んでいるときは動かない
+            if (PlayerState == State.Dead)
+            {
+                _rigidbody.velocity = Vector2.zero;
+                return;
+            }
 
-			var controller = GameController.Instance;
+            var controller = GameController.Instance;
 
-			// 移動処理とポーズ処理
-			Vector3 tryMove = Vector3.zero;
-			if (controller.GetConnectFlag())
-			{
-				tryMove = ControllerControl(controller);
-				//アニメーション切り替え
-				gameObject.GetComponent<PlayerAnimController>().ChangeAnim(_direction, _waitCount);
-			}
-			else
-			{
-				tryMove = KeyboardControl();
-				//アニメーション切り替え
-				gameObject.GetComponent<PlayerAnimController>().ChangeAnim(_direction, _waitCount);
+            // 移動処理とポーズ処理
+            Vector3 tryMove = Vector3.zero;
+            if (controller.GetConnectFlag())
+            {
+                tryMove = ControllerControl(controller);
+                //アニメーション切り替え
+                gameObject.GetComponent<PlayerAnimController>().ChangeAnim(_direction, _waitCount);
+            }
+            else
+            {
+                tryMove = KeyboardControl();
+                //アニメーション切り替え
+                gameObject.GetComponent<PlayerAnimController>().ChangeAnim(_direction, _waitCount);
 
-			}
+            }
 
-			//待機時間経過
-			if (tryMove == Vector3.zero)
-			{
-				_waitCount += Time.deltaTime;
-			}
-			else
-			{
-				_waitCount = 0;
-			}
+            //待機時間経過
+            if (tryMove == Vector3.zero)
+            {
+                _waitCount += Time.deltaTime;
+            }
+            else
+            {
+                _waitCount = 0;
+            }
 
-			//移動
-			_rigidbody.velocity = Vector3.ClampMagnitude(tryMove, 1f) * _moveSpeed;
-		}
+            //移動
+            _rigidbody.velocity = Vector3.ClampMagnitude(tryMove, 1f) * _moveSpeed;
+        }
 
-		/// <summary>
-		/// キーボードでの操作
-		/// </summary>
-		virtual protected Vector3 KeyboardControl()
-		{
-			Vector3 tryMove = Vector3.zero;
+        /// <summary>
+        /// キーボードでの操作
+        /// </summary>
+        virtual protected Vector3 KeyboardControl()
+        {
+            Vector3 tryMove = Vector3.zero;
 
-			if (Input.GetKey(KeyCode.LeftArrow))
-			{
-				tryMove += Vector3Int.left;
-				_direction = Direction.Left;
-			}
-			if (Input.GetKey(KeyCode.RightArrow))
-			{
-				tryMove += Vector3Int.right;
-				_direction = Direction.Right;
-			}
-			if (Input.GetKey(KeyCode.UpArrow))
-			{
-				tryMove += Vector3Int.up;
-				_direction = Direction.Front;
-			}
-			if (Input.GetKey(KeyCode.DownArrow))
-			{
-				tryMove += Vector3Int.down;
-				_direction = Direction.Back;
-			}
-
-			return tryMove;
-		}
-
-		/// <summary>
-		/// コントローラーの操作
-		/// </summary>
-		virtual protected Vector3 ControllerControl(GameController con)
-		{
-			Vector3 tryMove = Vector3.zero;
-
-			if (con.Move(Direction.Left))
-			{
-				tryMove += Vector3Int.left;
-				_direction = Direction.Left;
-			}
-			if (con.Move(Direction.Right))
-			{
-				tryMove += Vector3Int.right;
-				_direction = Direction.Right;
-			}
-			if (con.Move(Direction.Front))
-			{
-				tryMove += Vector3Int.up;
-                _direction = Direction.Front;            
-			}
-			if (con.Move(Direction.Back))
-			{
-				tryMove += Vector3Int.down;
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                tryMove += Vector3Int.left;
+                _direction = Direction.Left;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                tryMove += Vector3Int.right;
+                _direction = Direction.Right;
+            }
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                tryMove += Vector3Int.up;
+                _direction = Direction.Front;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                tryMove += Vector3Int.down;
                 _direction = Direction.Back;
             }
 
-			return tryMove;
-		}
+            return tryMove;
+        }
 
-		/// <summary>
-		/// 死亡
-		/// </summary>
-		public void Dead(bool retry = true)
-		{
-			_playerState = State.Dead;
+        /// <summary>
+        /// コントローラーの操作
+        /// </summary>
+        virtual protected Vector3 ControllerControl(GameController con)
+        {
+            Vector3 tryMove = Vector3.zero;
 
-			
+            if (con.Move(Direction.Left))
+            {
+                tryMove += Vector3Int.left;
+                _direction = Direction.Left;
+            }
+            if (con.Move(Direction.Right))
+            {
+                tryMove += Vector3Int.right;
+                _direction = Direction.Right;
+            }
+            if (con.Move(Direction.Front))
+            {
+                tryMove += Vector3Int.up;
+                _direction = Direction.Front;
+            }
+            if (con.Move(Direction.Back))
+            {
+                tryMove += Vector3Int.down;
+                _direction = Direction.Back;
+            }
 
-			if (retry)
-			{
-				InGameManager.Instance.StageOver();
-			}
-		}
+            return tryMove;
+        }
 
-		/// <summary>
-		/// 復活
-		/// </summary>
-		public void Reborn()
-		{
-			_playerState = State.Alive;
-			var renderer = GetComponent<Renderer>();
-			renderer.sortingOrder = 0;
-			//復帰時のアニメーション変更（デフォルト下向き）
-			gameObject.GetComponent<PlayerAnimController>().ChangeAnim(PlayerAnimController.ANIMATION_ID.Back);
-		}
+        /// <summary>
+        /// 死亡
+        /// </summary>
+        public void Dead(bool retry = true)
+        {
+            if (_playerState == State.Dead) return;
 
-		public void Goal()
-		{
-			var collider = GetComponent<BoxCollider2D>();
-			collider.enabled = false;
+            _playerState = State.Dead;
 
-			var renderer = GetComponent<SpriteRenderer>();
-			var layer = renderer.sortingOrder;
-			renderer.sortingOrder = layer - 2;
-		}
-	}
+            if (retry)
+            {
+                InGameManager.Instance.StageOver();
+            }
+        }
+
+        /// <summary>
+        /// 復活
+        /// </summary>
+        public void Reborn()
+        {
+            _playerState = State.Alive;
+            var renderer = GetComponent<Renderer>();
+            renderer.sortingOrder = 0;
+            //復帰時のアニメーション変更（デフォルト下向き）
+            gameObject.GetComponent<PlayerAnimController>().ChangeAnim(PlayerAnimController.ANIMATION_ID.Back);
+        }
+
+        public void Goal()
+        {
+            var collider = GetComponent<BoxCollider2D>();
+            collider.enabled = false;
+
+            var renderer = GetComponent<SpriteRenderer>();
+            var layer = renderer.sortingOrder;
+            renderer.sortingOrder = layer - 2;
+        }
+    }
 }
